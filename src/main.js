5eebf4172893ff7235cac656df17f993c80c87b6
XuJiazhi <9138382+jiafaCompiler@user.noreply.gitee.com>
2021-06-25 18:47:48 +0800

    调整了初始页面大小，使一开始不会显示拖拽条，让控制台脱离了flex，绝对定位贴底

--- src/main.js
+++ src/main.js
@@ -54,8 +54,8 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   mainWindow = new BrowserWindow({
-    width: 800,
-    height: 600,
+    width: 1000,
+    height: 800,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
